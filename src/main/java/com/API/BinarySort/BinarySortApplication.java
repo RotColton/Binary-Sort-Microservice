bb7459940608ccfdf2db364160047cdcc94200d3
RotColton <maromidomingo@gmail.com>
2024-12-07 19:23:20 +0100

    API documentation

---
+++ src/main/java/com/API/BinarySort/BinarySortApplication.java
@@ -0,0 +1,13 @@
+package com.API.BinarySort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinarySortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinarySortApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/API/BinarySort/BinarySortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API.BinarySort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinarySortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
